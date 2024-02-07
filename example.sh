54efbe62b23bbbbb7250e12f3cdcda046df484ec
pradeepgowda1999 <158177273+pradeepgowda1999@users.noreply.github.com>
2024-02-08 00:56:27 +0530

    Update example.sh

--- example.sh
+++ example.sh
@@ -1,6 +1,8 @@
 if [ "$BUILD_STATUS" = "SUCCESS" ]; then
   echo "Build was successful, running additional steps..."
   # Add additional commands here
+  free -m
+  df -kh .
 else
   echo "Build failed, skipping additional steps."
 fi
